71120b86dcd257b87a13d4ef4dd2812e5218ca4e
gaffleckp8 <geoff.affleck@produce8.com>
2021-11-24 16:15:02 -0800

    Rename stack

--- default-alarms/bin/default-alarms.ts
+++ default-alarms/bin/default-alarms.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { DefaultAlarmsStack } from '../lib/default-alarms-stack';
+import {  DefaultAlarmsStack } from '../lib/default-alarms-stack';
 
 const app = new cdk.App();
-new DefaultAlarmsStack(app, 'DefaultAlarmsStack3', {
+const core = new DefaultAlarmsStack(app, 'DefaultAlarmsStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +19,5 @@ new DefaultAlarmsStack(app, 'DefaultAlarmsStack3', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+
